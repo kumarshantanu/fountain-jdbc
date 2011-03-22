41d5aadac654d43ea445eaf3025dca5349168b33
kumarshantanu <none@none>
2011-03-23 00:13:04 +0530

    include transaction namespace in run-tests script

--- src/test/script/runtests.clj
+++ src/test/script/runtests.clj
@@ -1,7 +1,14 @@
 (ns runtests
-  (:use org.bituf.fountain.test-jdbc)
+  (:require
+    [org.bituf.fountain.test-jdbc :as jd]
+    [org.bituf.fountain.test-transaction :as tx])
   (:use clojure.test))
 
 
+;; enable logging
+;(org.apache.log4j.BasicConfigurator/configure)
+
+
 (run-tests
-  'org.bituf.fountain.test-jdbc)
+  'org.bituf.fountain.test-jdbc
+  'org.bituf.fountain.test-transaction)
