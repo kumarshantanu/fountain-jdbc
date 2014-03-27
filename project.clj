66a1aa994d487125a8eb68b201211197c4f3bcc5
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-03-28 01:06:30 +0530

    bump versions - SpringJDBC-4.0.3 and Clojure-1.6.0 for test

--- project.clj
+++ project.clj
@@ -11,13 +11,13 @@
   :javac-options {:destdir "target/classes/"
                   :source  "1.5"
                   :target  "1.5"}
-  :dependencies [[org.springframework/spring-jdbc "4.0.2.RELEASE"]]
+  :dependencies [[org.springframework/spring-jdbc "4.0.3.RELEASE"]]
   :profiles {:dev {:dependencies [[oss-jdbc "0.8.0"]
                                   [clj-dbcp "0.8.1"]
                                   [net.mikera/cljunit "0.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :aliases {"dev" ["with-profile" "dev,1.5"]
             "all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6"]}
   :global-vars {*warn-on-reflection* true}
